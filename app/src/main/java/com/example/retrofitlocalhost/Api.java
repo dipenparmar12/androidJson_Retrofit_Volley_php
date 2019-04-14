a893ec0ebbe995748c17657a4555ef2ed54903a5
dipends <dipensavji@gmail.com>
2019-04-14 17:22:10 +0530

    OnBtn CLick Working with RecyclerVIew

--- app/src/main/java/com/example/retrofitlocalhost/Api.java
+++ app/src/main/java/com/example/retrofitlocalhost/Api.java
@@ -11,9 +11,11 @@ import retrofit2.http.Query;
 
     // localhost == 10.0.2.2 !Important
 
-* "http://10.0.2.2/getdata.php"
-* "http://10.0.2.2/student.php"
+
 * "http://10.0.2.2/students.php"
+
+*  http://127.0.0.1/sql.php?opration=select
+*  http://127.0.0.1/sql.php?opration=select&id=1
 *
 */
 
@@ -27,54 +29,12 @@ public interface Api {
     Call<ArrayList<StudentsJson>> getStudentsList();
 
 
-    @GET("sql.php?opration=select")
+    @GET("sql.php")
     Call<ArrayList<StudentsJson>> getStudentById(@Query("id") String id);
 
 
-    @GET("sql.php?opration=select")
-    Call<ArrayList<StudentsJson>> getAllStudents(@Query("opration") String opration);
+//    @GET("sql.php")
+//    Call<ArrayList<StudentsJson>> getAllStudents();
 
 }
 
-
-
-
-
-
-
-/*
- * URL: https://api.github.com/
- * https://api.github.com/users/{user}
- * https://api.github.com/users/{user}/repos
- * URL: https://api.github.com/
- * https://api.github.com/users/taylorotwell
- * https://api.github.com/users/taylorotwell/repos
- * https://api.github.com/search/repositories?q=flexsass
- * https://api.github.com/repos/dipenparmar12/FlexSass
- * Download Uri
- * https://github.com/userName/RepoName/archive/master.zip
- * svn_url+"/archive/"+default_branch+".zip"
- */
-
-
-//public interface GitApi {
-//
-//    public final String BASE_URL = "https://api.github.com/";
-//
-//    @GET("users/{gitUser}")
-//    Call<GitUserDetailJson> getUserDetail(@Path("gitUser") String gitUser);
-//
-//    @GET("users/{gitUser}/repos")
-//    Call<ArrayList<GitRepoDetailJson>> getReposByUser(@Path("gitUser") String gitUser);
-//
-//    @GET("search/repositories")
-//    Call<RepoListItemsJson> getReposBySearchPara(@Query("q") String SearchQry);
-//
-////    @GET("search/repositories?q={SearchQry}")
-////    Call<ArrayList<GitUserDetailJson>> getReposBySearchPara(@Path("SearchQry") String SearchQry);
-//
-//}
-
-//
-//    @GET("group/{id}/users")
-//    Call<List<User>> groupList(@Path("id") int groupId, @Query("sort") String sort);
--- app/src/main/java/com/example/retrofitlocalhost/InsertStudent.java
+++ app/src/main/java/com/example/retrofitlocalhost/InsertStudent.java
@@ -2,7 +2,12 @@ package com.example.retrofitlocalhost;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -25,8 +30,39 @@ public class InsertStudent extends AppCompatActivity implements Callback<ArrayLi
 
         retrofit = new Retrofit.Builder().baseUrl(Api.URL).addConverterFactory(GsonConverterFactory.create()).build();
         api = retrofit.create(Api.class);
-        getStudentById = api.getStudentById("1");
+
+        getStudentById = api.getStudentsList();
         getStudentById.enqueue(this);
+
+
+        Button button = findViewById(R.id.btn_select_id);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                getStudentById = api.getStudentById("3");
+                getStudentById.enqueue(new Callback<ArrayList<StudentsJson>>() {
+                    @Override
+                    public void onResponse(Call<ArrayList<StudentsJson>> call, Response<ArrayList<StudentsJson>> response) {
+                        Log.e("dipen", "onResponse: "+response.body() );
+                        RecyclerView recyclerView;
+                        RecyclerView.Adapter adapter;
+
+                        recyclerView = findViewById(R.id.recyclerView);
+                        recyclerView.setLayoutManager(new LinearLayoutManager(InsertStudent.this));
+
+                        adapter = new StudentAdapter(InsertStudent.this, response.body());
+                        recyclerView.setAdapter(adapter);
+                    }
+
+                    @Override
+                    public void onFailure(Call<ArrayList<StudentsJson>> call, Throwable t) {
+                        Log.e("dipen", "onFailure: "+t.toString() );
+                    }
+                });
+            }
+        });
+
 
     }
 
@@ -34,6 +70,16 @@ public class InsertStudent extends AppCompatActivity implements Callback<ArrayLi
     @Override
     public void onResponse(Call<ArrayList<StudentsJson>> call, Response<ArrayList<StudentsJson>> response) {
         Log.e("dipen", "onResponse: "+response.body());
+
+        RecyclerView recyclerView;
+        RecyclerView.Adapter adapter;
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        adapter = new StudentAdapter(this,response.body());
+        recyclerView.setAdapter(adapter);
+
     }
 
     @Override
