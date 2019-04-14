9e071e48bf70a7fe57e59c6235b18844e1d9baed
dipends <dipensavji@gmail.com>
2019-04-14 17:51:12 +0530

    Select Student By Id ( Using Spinner )

--- app/src/main/java/com/example/retrofitlocalhost/ListStudentInfo.java
+++ app/src/main/java/com/example/retrofitlocalhost/ListStudentInfo.java
@@ -1,15 +1,20 @@
 package com.example.retrofitlocalhost;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -17,11 +22,17 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class InsertStudent extends AppCompatActivity implements Callback<ArrayList<StudentsJson>> {
+public class ListStudentInfo extends AppCompatActivity implements Callback<ArrayList<StudentsJson>> {
 
     Api api;
     Retrofit retrofit;
     Call<ArrayList<StudentsJson>> getStudentById;
+
+    RecyclerView recyclerView;
+    RecyclerView.Adapter adapter;
+
+    Spinner spinner;
+    ArrayAdapter spinnerArrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,19 +51,14 @@ public class InsertStudent extends AppCompatActivity implements Callback<ArrayLi
             @Override
             public void onClick(View v) {
 
-                getStudentById = api.getStudentById("3");
+                getStudentById = api.getStudentById(spinner.getSelectedItem()+"");
                 getStudentById.enqueue(new Callback<ArrayList<StudentsJson>>() {
                     @Override
                     public void onResponse(Call<ArrayList<StudentsJson>> call, Response<ArrayList<StudentsJson>> response) {
                         Log.e("dipen", "onResponse: "+response.body() );
-                        RecyclerView recyclerView;
-                        RecyclerView.Adapter adapter;
 
-                        recyclerView = findViewById(R.id.recyclerView);
-                        recyclerView.setLayoutManager(new LinearLayoutManager(InsertStudent.this));
+                        bindData(ListStudentInfo.this,response.body());
 
-                        adapter = new StudentAdapter(InsertStudent.this, response.body());
-                        recyclerView.setAdapter(adapter);
                     }
 
                     @Override
@@ -71,14 +77,17 @@ public class InsertStudent extends AppCompatActivity implements Callback<ArrayLi
     public void onResponse(Call<ArrayList<StudentsJson>> call, Response<ArrayList<StudentsJson>> response) {
         Log.e("dipen", "onResponse: "+response.body());
 
-        RecyclerView recyclerView;
-        RecyclerView.Adapter adapter;
+        bindData(this,response.body());
 
-        recyclerView = findViewById(R.id.recyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        List studentIdList = new ArrayList();
+        for(StudentsJson studentJson:response.body()){
+            studentIdList.add(studentJson.getId());
+        }
 
-        adapter = new StudentAdapter(this,response.body());
-        recyclerView.setAdapter(adapter);
+        spinner = findViewById(R.id.spinner2);
+        spinnerArrayAdapter = new ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item,studentIdList);
+        spinner.setAdapter(spinnerArrayAdapter);
+
 
     }
 
@@ -86,5 +95,15 @@ public class InsertStudent extends AppCompatActivity implements Callback<ArrayLi
     public void onFailure(Call<ArrayList<StudentsJson>> call, Throwable t) {
         Log.e("dipen", "onFailure: "+t.toString() );
     }
+
+
+
+    public void bindData(Context c,Object object){
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(ListStudentInfo.this));
+        adapter = new StudentAdapter(c, (ArrayList<StudentsJson>) object);
+        recyclerView.setAdapter(adapter);
+    }
+
 
 }
